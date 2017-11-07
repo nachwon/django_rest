446d73f6e17425f8172eef2853061fe0e94994bf
Che1 <nachwon@gmail.com>
2017-11-07 11:23:29 +0900

    mixin views added

--- tutorial/snippets/urls/__init__.py
+++ tutorial/snippets/urls/__init__.py
@@ -1,8 +1,9 @@
 from django.conf.urls import include, url
 
-from . import cbv, fbv
+from . import cbv, fbv, cbv_mixins
 
 urlpatterns = [
     url(r'^fbv/', include(fbv, namespace='fbv')),
     url(r'^cbv/', include(cbv, namespace='cbv')),
+    url(r'^cbv-mixins/', include(cbv_mixins, namespace='cbv_mixins')),
 ]
